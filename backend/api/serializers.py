a3dca01bcb3bce3dd52f30b609a948bb457032c0
zuzuz <duda.zuzanna7@gmail.com>
2025-04-09 13:58:08 +0200

    some basic serializers for important data

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
-
-# class RestaurantSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Restaurant
-#         fields = '__all__'
+from .models import *
 
 class RestaurantSerializer(serializers.Serializer):
     id = serializers.CharField(source="place_id")
@@ -12,6 +8,43 @@ class RestaurantSerializer(serializers.Serializer):
     address = serializers.CharField(source="vicinity")
     latitude = serializers.FloatField(source="geometry.location.lat")
     longitude = serializers.FloatField(source="geometry.location.lng")
-    type = serializers.ListField(child=serializers.CharField(), required=False)  # For cuisine types
-    reviews = serializers.ListField(child=serializers.DictField(), required=False)  # Reviews
-    opening_hours = serializers.ListField(child=serializers.CharField(), required=False)  # Opening hours
+    type = serializers.ListField(
+        child=serializers.CharField(), required=False)  # For cuisine types
+    reviews = serializers.ListField(
+        child=serializers.DictField(), required=False)  # Reviews
+    opening_hours = serializers.ListField(
+        child=serializers.CharField(), required=False)  # Opening hours
+
+class ClientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clients
+        fields = '__all__'
+
+class FavoritesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorites
+        fields = '__all__'
+
+
+class LoginAttemptsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoginAttempts
+        fields = '__all__'
+
+
+class PreferencesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Preferences
+        fields = '__all__'
+
+
+class RatingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ratings
+        fields = '__all__'
+
+
+class RestaurantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurants
+        fields = '__all__'
